18a614a6ada6f381f23637eedc38c20f82c80dc2
Leinu <leinum@bitbucket.org>
2019-03-14 17:03:25 +0200

    Added search functionalities

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
-
-file = open("oskarid.csv")
-data = csv.reader(file)
 
 app = Flask(__name__)
 @app.route('/')
 def gen_table():
+    file = open("oskarid.csv")
+    data = csv.reader(file)
     return render_template('index.html', data=data)
+@app.route('/search', methods=["GET","POST"])
+def search():
+    file = open("oskarid.csv")
+    data = csv.reader(file)
+    key = request.args.get('key')
+    sieve = request.args.get('filter')
+    return render_template('search.html', data=data, key=key, sieve=sieve)
 
